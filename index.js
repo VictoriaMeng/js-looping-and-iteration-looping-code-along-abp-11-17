8b4326cd09f8f7187827ffe2dda4b2cc97cd2cd0
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 02:57:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function printBadges(names) {
   for (let counter = 0; counter < names.length; counter++) {
     console.log(`Welcome ${names[counter]}! You are employee #${counter + 1}.`);
   }
-
+  return names;
 }
 
 printBadges();
