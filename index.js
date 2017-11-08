4e9243140aa6595373879715d2b148c3709f9e2e
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 02:56:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const names = ['Ada', 'Brendan', 'Ali']
+const names = []
 
 function printBadges() {
   for (let counter = 0; counter < names.length; counter++) {
