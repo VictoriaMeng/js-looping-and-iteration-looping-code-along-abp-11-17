cdb26edd8dc1d62b13a8426e74c9c4ff6d13bb64
Victoria Meng <nights1ststar@protonmail.com>
2017-11-08 02:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 const names = ['Ada', 'Brendan', 'Ali']
 
-function printBadges() {
+function printBadges(names) {
   for (let counter = 0; counter < names.length; counter++) {
     console.log(`Welcome ${names[counter]}! You are employee #${counter + 1}.`);
   }
